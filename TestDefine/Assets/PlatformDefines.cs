fe375de1e64f80930b8d48f0efaea10c87d19883
mingchaoyan <mingchaoyan@gmail.com>
2015-06-09 15:11:52 +0800

    add platformdefines

---
+++ TestDefine/Assets/PlatformDefines.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDefines : MonoBehaviour
+{
+	void Start ()
+	{
+		
+		#if UNITY_EDITOR
+		Debug.Log ("Unity Editor");
+		#endif
+		
+		#if UNITY_IPHONE
+		Debug.Log("Iphone");
+		#endif
+		
+		#if UNITY_STANDALONE_OSX
+		Debug.Log ("Stand Alone OSX");
+		#endif
+		
+		#if UNITY_STANDALONE_WIN
+		Debug.Log("Stand Alone Windows");
+		#endif
+		
+	}          
+}
+
