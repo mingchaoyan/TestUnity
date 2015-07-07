d9835a72b8a8bccb2e3cc064c009ad6cb6ffd59d
mingchaoyan <mingchaoyan@gmail.com>
2015-07-07 15:52:16 +0800

    创建脚本测试

---
+++ TestNavMesh/Assets/RunTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunTest : MonoBehaviour
+{
+	public Transform targetObject = null;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (targetObject != null)
+			GetComponent<NavMeshAgent> ().destination = targetObject.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
