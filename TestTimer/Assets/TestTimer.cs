da47e0464dd86b762a3cf3dcdea8ca746e0aa276
mingchaoyan <mingchaoyan@gmail.com>
2015-07-08 10:24:30 +0800

    add TestTimer

---
+++ TestTimer/Assets/TestTimer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTimer : MonoBehaviour
+{
+	public float time = 2;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		time -= Time.deltaTime;
+		if (time <= 0) {
+			Debug.Log ("time's up!");
+			time = 2;
+		}
+	}
+}
