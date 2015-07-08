ac8b6f6c8ea8250814fdef0c870da07f57b2e25c
mingchaoyan <mingchaoyan@gmail.com>
2015-07-08 10:22:31 +0800

    add TestLogCallback

---
+++ TestLogCallback/Assets/TestLogCallback.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;  
+using System.Collections;  
+
+public class TestLogCallback : MonoBehaviour
+{  
+	string output = "";//日志输出信息  
+	string stack = "";//堆栈跟踪信息  
+	string logType = "";//日志类型  
+	int tp = 0;  
+	
+	//打印日志信息  
+	void Update ()
+	{  
+		Debug.Log ("output:" + output);  
+		Debug.Log ("stack:" + stack);  
+		Debug.Log ("logType:" + logType);  
+		Debug.Log ("tp:" + (tp++));  
+	}  
+	void OnEnable ()
+	{  
+		//注册委托  
+		Application.RegisterLogCallback (MyCallback);  
+	}  
+	void OnDisable ()
+	{  
+		//取消委托  
+		Application.RegisterLogCallback (null);  
+	}  
+	//委托方法  
+	//方法名字可以自定义，但方法的参数类型要符合Application.LogCallback中的参数类型  
+	void MyCallback (string logString, string stackTrace, LogType type)
+	{  
+		output = logString;  
+		stack = stackTrace;  
+		logType = type.ToString ();  
+	}  
+} 
