0bb0ec71c568e2b152ac45e8ca0e81cbaeea8aec
mingchaoyan <mingchaoyan@gmail.com>
2015-07-08 23:06:44 +0800

    add TestTouch

---
+++ TestTouch/Assets/FingerCount.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FingerCount : MonoBehaviour
+{
+	public int fingerCount;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
+				fingerCount ++;
+			}
+			if (fingerCount > 0) {
+				print ("User has " + fingerCount + "finger(s) touching the screen");
+			}
+		}
+	}
+
+	void OnGUI ()
+	{
+		GUILayout.Label (fingerCount + "");
+	}
+}
---
+++ TestTouch/Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+	public float speed = 0.01f;
+
+	void Update ()
+	{
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			Vector2 touchDeltaPostion = Input.GetTouch (0).deltaPosition;
+			transform.Translate (touchDeltaPostion.x * speed, touchDeltaPostion.y * speed, 0);
+		}
+		print ("speed" + speed);
+	}
+
+	void OnGUI ()
+	{
+		GUILayout.Label ("speed: " + speed + "");
+	}
+}
