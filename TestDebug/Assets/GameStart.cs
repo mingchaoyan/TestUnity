a7e673f81cb38646a072d1c6d03f3e7c8acc2873
mingchaoyan <mingchaoyan@gmail.com>
2015-07-08 10:23:51 +0800

    add TestDebug

---
+++ TestDebug/Assets/GameStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log ("1111");
+		Debug.Log ("222222");
+		Debug.Log ("33333");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
